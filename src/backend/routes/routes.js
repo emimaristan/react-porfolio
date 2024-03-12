ba750985451f024fe378b3c53cecbb45aab07b6f
emimaristan <maristan97@gmail.com>
2024-03-12 06:34:09 -0300

    Skills routes completed

--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -1,61 +1,68 @@
-const express = require('express');
-const Model = require('../model/skillsModel');
+/* eslint-disable no-undef */
+const express = require("express");
+const skillsModel = require("../model/skillsModel");
+const educationModel = require("../model/educationModel");
 
 const router = express.Router();
 
-router.post('/post', async (req, res) => {
-    const newTask = new Model({
-        task: req.body.task,
-        status: true,
-    });
+//SKILLS
+router.post("/skills/post", async (req, res) => {
+  console.log(req.body);
+  const newSkill = new skillsModel({
+    type: req.body.type,
+    technology: req.body.technology,
+    level: req.body.level,
+  });
 
-    try {
-        const data = await newTask.save();
-        res.status(201).json({data: data, status: 201});
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
+  try {
+    const data = await newSkill.save();
+    res.status(201).json({ data: data, status: 201 });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.get('/getAll', async(req, res) => {
-    try {
-        const data = await Model.find();
-        res.status(200).json({data: data, status: 200});
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+router.get("/skills", async (req, res) => {
+  try {
+    const skills = await skillsModel.find().select("-_id -__v");
+    res.status(200).json({ data: skills, status: 200 });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.get('/getOne/:id', async (req, res) => {
-    try {
-        const data = await Model.findById(req.params.id);
-        res.status(200).json({data: data, status: 200});
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+router.get("/skills/:id", async (req, res) => {
+  try {
+    const skill = await skillsModel.findById(req.params.id).select("-_id -__v");
+    res.status(200).json({ data: skill, status: 200 });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.patch('/update/:id', async (req, res) => {
-    try {
-        console.log(req.body);
-        const id = req.params.id;
-        const options = {new: true};
-        
-        const data = await Model.findByIdAndUpdate(id, req.body, options);
-        res.status(204).json({data: data, status: 204});
+router.patch("/skills/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const options = { new: true, select: "-_id -__v" };
 
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
+    const skill = await skillsModel.findByIdAndUpdate(id, req.body, options);
+    res.status(200).json({ data: skill, status: 204 });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const data = Model.findByIdAndDelete(req.params.id);
-        res.status(200).json(data.name);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
+router.delete("/skills/:id", async (req, res) => {
+  try {
+    const skill = await skillsModel.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({
+        message: `The skill ${skill.technology} was deleted successfully`,
+      });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
